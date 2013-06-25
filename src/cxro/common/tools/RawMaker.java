56077b61dc9bf92e59c9a37bbcc6acb6d8be2a51
wcork <willfcork@gmail.com>
2013-06-25 13:32:18 -0700

    Added new header and Javadocs to methods.

--- src/cxro/common/tools/RawMaker.java
+++ src/cxro/common/tools/RawMaker.java
@@ -1,25 +1,42 @@
-/*
- * This is a library designed to write .raw single files or .raw sequences.
- */
-package cxro.common.tools;
-
-import java.io.*;
+// RawMaker : This is a library designed to write .raw single files or .raw sequences.
+//
+///////////////////////////////////////////////////////////////////////////////
+/// @mainpage
+/// Methods for writing a single or sequence of images to a .raw file
+///
+/// Written June 2013 by William Cork based on original ipvcam library from Bob Gunion.
+///
+///
+/// Copyright (c) 2013 Lawrence Berkeley National Laboratory
+///////////////////////////////////////////////////////////////////////////////
 
 /**
  *
  * @author wcork
  */
+
+package cxro.common.tools;
+
+import java.io.*;
+
 public class RawMaker
 {
     OutputStream output = null;
     boolean sequenceStarted = false;
 
+    /**
+     * Writes a .raw file with the given filename of the given buffer.
+     * 
+     * Does not append '.raw' to the end of filename.
+     * 
+     * @param filename
+     * @param buffer
+     * @throws FileNotFoundException
+     * @throws IOException 
+     */
     public static void createSingleImage(String filename, byte[] buffer) throws FileNotFoundException, IOException
     {
         OutputStream output = new BufferedOutputStream(new FileOutputStream(filename));
-        /*
-         * write the frame to the file
-         */
         for (int x = 0; x < buffer.length; x++)
         {
             output.write(buffer[x]);
@@ -27,12 +44,29 @@ public class RawMaker
         output.close();
     }
 
+    /**
+     * Begins an output buffer stream to a file called filename.
+     * 
+     * Does not append '.raw' to the end of filename.
+     * 
+     * @param filename
+     * @throws FileNotFoundException 
+     */
     public void startImageSequence(String filename) throws FileNotFoundException
     {
         sequenceStarted = true;
         output = new BufferedOutputStream(new FileOutputStream(filename));
     }
 
+    /**
+     * Adds an image from the param buffer to the image sequence.
+     * 
+     * Must call startImageSequence before this method
+     * otherwise, an IOException is thrown.
+     * 
+     * @param buffer
+     * @throws IOException 
+     */
     public void insertImageIntoSequence(byte[] buffer) throws IOException
     {
         if(!sequenceStarted)
@@ -45,6 +79,12 @@ public class RawMaker
         }
     }
 
+    /**
+     * Completes the image sequence and closes the file.
+     * 
+     * 
+     * @throws IOException 
+     */
     public void endImageSequence() throws IOException
     {
         sequenceStarted = false;
